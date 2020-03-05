d3e196ea03db985811d4edd20d74edc13c60d06e
vibhor-garg-98 <vibhor.garg@successive.tech>
2020-03-05 21:02:38 +0530

    feat: add files(#40535)

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import Input from './style'
+import Input from './style';
+import PropTypes from 'prop-types';
 
 const TextField = (props) => {
-
-   // console.log('props are :::::::',props);
-
-     return <Input type="text" value={props.value} disabled={(props.disabled)} />
+  const { onChange, value } = props
+  return <Input type="text" onChange={onChange} value={value} />
 }
+
+TextField.propTypes = {
+  error: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
+TextField.defaultProps = {
+  error: '',
+};
+
 export default TextField;
--- src/configs/constants.js
+++ src/configs/constants.js
@@ -2,3 +2,46 @@ export const PUBLIC_IMAGE_FOLDER = '/images/';
 export const DEFAULT_BANNER_IMAGE = 'banners/default.png';
 
 export const banners = ['cloud.jpg','dns-server.png','js.jpg','full-stack-web-development.jpg','load-balancer.png'];
+
+
+export const selectOptions = [
+  {
+    label: 'Cricket',
+    value: 'cricket'
+  },
+  {
+    label: 'Football',
+    value: 'football'
+  }
+]
+
+export const radioOptionsCricket = [
+  {
+    label: 'Bowler',
+    value: 'bowler'
+  },
+  {
+    label: 'Batsman',
+    value: 'batsman'
+  },
+  {
+    label: 'Wicket Keeper',
+    value: 'wicket Keeper'
+  },
+  {
+    label: 'All rounder',
+    value: 'all rounder'
+  }
+]
+
+
+export const radioOptionsFootball = [
+  {
+    label: 'Defender',
+    value: 'defender'
+  },
+  {
+    label: 'Striker',
+    value: 'striker'
+  }
+]
