45ba053aeb641ebaa8f5eb270024a9af8e000a7b
vibhor-garg-98 <vibhor.garg@successive.tech>
2020-03-22 22:19:14 +0530

    feat: feature(#41046)

--- src/pages/Trainee/TraineeList.jsx
+++ src/pages/Trainee/TraineeList.jsx
@@ -1,8 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 import AddDialog from './components/AddDialog/AddDialog';
 import trainee from './data/trainee';
+import Table from './components/Table/Table';
+
+const useStyles = (theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    margin: theme.spacing(2, 0, 2),
+  },
+});
 
 class Trainee extends Component {
   constructor(props) {
@@ -22,31 +32,52 @@ class Trainee extends Component {
   };
 
   onSubmit = (data) => {
-
     this.setState({ open: false }, () => { console.log(data); });
   };
 
   render() {
     const { open } = this.state;
+    const { classes } = this.props;
 
     return (
       <>
-        <Button variant="outlined" color="primary" onClick={this.onClick}>
-          ADD TRAINEE
-        </Button>
+        <div className={classes.root}>
+          <Button variant="outlined" color="primary" onClick={this.onClick}>
+            ADD TRAINEE
+          </Button>
+        </div>
+        <Table
+          id="id"
+          data={trainee}
+          columns={
+            [
+              {
+                field: 'name',
+                label: 'Name',
+                align: 'center',
+              },
+              {
+                field: 'email',
+                label: 'Email Address',
+              },
+            ]
+          }
+        />
         <AddDialog onClose={this.onClose} onSubmit={() => this.onSubmit} open={open} />
         <ul>
           {
             trainee && trainee.length && trainee.map((element) => (
-              <li>
-                <Link to={`/Trainee/${element.id}`}>{element.name}</Link>
-              </li>
+              <Fragment key={element.id}>
+                <li key={element.id}>
+                  <Link to={`/Trainee/${element.id}`}>{element.name}</Link>
+                </li>
+              </Fragment>
             ))
           }
         </ul>
       </>
-    )
+    );
   }
 }
 
-export default Trainee;
+export default withStyles(useStyles)(Trainee);
