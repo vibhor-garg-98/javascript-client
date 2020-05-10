7e1a32d7121b1e31231f4fb13a0980ebd2beaf8f
vibhor-garg-98 <vibhor.garg@successive.tech>
2020-05-10 13:09:21 +0530

    feat: resolve suggesstion (#feature/41046)

--- src/pages/Trainee/TraineeList.jsx
+++ src/pages/Trainee/TraineeList.jsx
@@ -1,8 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import AddDialog from './components/AddDialog/AddDialog';
 import trainee from './data/trainee';
+import Table from './components/Table/Table';
+
+
+const useStyles = (theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    margin: theme.spacing(2, 0, 2),
+  },
+});
 
 class Trainee extends Component {
   constructor(props) {
@@ -27,19 +39,45 @@ class Trainee extends Component {
 
   render() {
     const { open } = this.state;
+    const { classes } = this.props;
 
     return (
       <>
-        <Button variant="outlined" color="primary" onClick={() => this.openDialog(true)}>
-          ADD TRAINEE
-        </Button>
-        <AddDialog onClose={() => this.openDialog(false)} onSubmit={() => this.onSubmit} open={open} />
+        <div className={classes.root}>
+          <Button variant="outlined" color="primary" onClick={() => this.openDialog(true)}>
+            ADD TRAINEE
+          </Button>
+        </div>
+        <Table
+          id="id"
+          data={trainee}
+          columns={
+            [
+              {
+                field: 'name',
+                label: 'Name',
+                align: 'center',
+              },
+              {
+                field: 'email',
+                label: 'Email Address',
+              },
+            ]
+          }
+        />
+        <AddDialog
+          onClose={() => this.openDialog(false)}
+          onSubmit={() => this.onSubmit}
+          open={open}
+        />
         <ul>
           {
             trainee && trainee.length && trainee.map((element) => (
-              <li>
-                <Link to={`/Trainee/${element.id}`}>{element.name}</Link>
-              </li>
+              <Fragment key={element.id}>
+                <li key={element.id}>
+                  <Link to={`/Trainee/${element.id}`}>{element.name}</Link>
+                </li>
+              </Fragment>
             ))
           }
         </ul>
@@ -47,5 +85,7 @@ class Trainee extends Component {
     );
   }
 }
-
-export default Trainee;
+Trainee.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+export default withStyles(useStyles)(Trainee);
