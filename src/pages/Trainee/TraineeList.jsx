e8b1495d825c47a0e773e8b018b6cd08a15df25e
vibhor-garg-98 <vibhor.garg@successive.tech>
2020-05-27 19:40:42 +0530

    feat: add sort and onclick eventon table(feature/41074)

--- src/pages/Trainee/TraineeList.jsx
+++ src/pages/Trainee/TraineeList.jsx
@@ -3,10 +3,10 @@ import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import moment from 'moment';
 import AddDialog from './components/AddDialog/AddDialog';
 import trainee from './data/trainee';
 import Table from './components/Table/Table';
-
 
 const useStyles = (theme) => ({
   root: {
@@ -22,6 +22,9 @@ class Trainee extends Component {
 
     this.state = {
       open: false,
+      selected: '',
+      orderBy: '',
+      order: '',
     };
   }
 
@@ -29,16 +32,28 @@ class Trainee extends Component {
     this.setState({ open: status });
   };
 
-  // onClose = () => {
-  //   this.setState({ open: false }, () => { console.log(this.state); });
-  // };
-
   onSubmit = (data) => {
     this.setState({ open: false }, () => { console.log(data); });
   };
 
+  handleSort = (field) => (event) => {
+    const { order } = this.state;
+    this.setState({
+      orderBy: field,
+      order: order === 'asc' ? 'desc' : 'asc',
+    });
+  }
+
+  handleSelect = (event, data) => {
+    this.setState({ selected: event.target.value }, () => console.log(data));
+  };
+
+  Format = (date) => moment(date).format('dddd, MMMM Do YYYY, h:mm:ss a')
+
+  Convert = (email) => email.toUpperCase()
+
   render() {
-    const { open } = this.state;
+    const { open, order, orderBy } = this.state;
     const { classes } = this.props;
 
     return (
@@ -61,14 +76,26 @@ class Trainee extends Component {
               {
                 field: 'email',
                 label: 'Email Address',
+                format: (value) => value && value.toUpperCase(),
+              },
+              {
+                field: 'createdAt',
+                label: 'Date',
+                align: 'right',
+                format: this.Format,
               },
             ]
           }
+          orderBy={orderBy}
+          order={order}
+          onSort={this.handleSort}
+          onSelect={this.handleSelect}
         />
-        <Button variant="outlined" color="primary" onClick={() => this.openDialog(true)}>
-          ADD TRAINEE
-        </Button>
-        <AddDialog onClose={() => this.openDialog(false)} onSubmit={() => this.onSubmit} open={open} />
+        <AddDialog
+          onClose={() => this.openDialog(false)}
+          onSubmit={() => this.onSubmit}
+          open={open}
+        />
 
         <ul>
           {
@@ -85,9 +112,241 @@ class Trainee extends Component {
     );
   }
 }
-       
+
 Trainee.propTypes = {
   classes: PropTypes.objectOf(PropTypes.string).isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']),
+  orderBy: PropTypes.string,
+};
+Trainee.defaultProps = {
+  orderBy: '',
+  order: 'asc',
 };
 
+
 export default withStyles(useStyles)(Trainee);
+
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Table from '@material-ui/core/Table';
+// import TableBody from '@material-ui/core/TableBody';
+// import TableCell from '@material-ui/core/TableCell';
+// import TableContainer from '@material-ui/core/TableContainer';
+// import TableHead from '@material-ui/core/TableHead';
+// import TableRow from '@material-ui/core/TableRow';
+// import TableSortLabel from '@material-ui/core/TableSortLabel';
+// import Paper from '@material-ui/core/Paper';
+
+// function createData(name, calories, fat) {
+//   return {
+//     name, calories, fat,
+//   };
+// }
+
+// const rows = [
+//   createData('Cupcake', 305, 3.7, 67, 4.3),
+//   createData('Donut', 452, 25.0, 51, 4.9),
+//   createData('Eclair', 262, 16.0, 24, 6.0),
+// ];
+
+// function descendingComparator(a, b, orderBy) {
+//   if (b[orderBy] < a[orderBy]) {
+//     return -1;
+//   }
+//   if (b[orderBy] > a[orderBy]) {
+//     return 1;
+//   }
+//   return 0;
+// }
+
+// function getComparator(order, orderBy) {
+//   return order === 'desc'
+//     ? (a, b) => descendingComparator(a, b, orderBy)
+//     : (a, b) => -descendingComparator(a, b, orderBy);
+// }
+
+// function stableSort(array, comparator) {
+//   const stabilizedThis = array.map((el, index) => [el, index]);
+//   stabilizedThis.sort((a, b) => {
+//     const order = comparator(a[0], b[0]);
+//     if (order !== 0) return order;
+//     return a[1] - b[1];
+//   });
+//   return stabilizedThis.map((el) => el[0]);
+// }
+
+// const headCells = [
+//   {
+//     id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)',
+//   },
+//   {
+//     id: 'calories', numeric: true, disablePadding: false, label: 'Calories',
+//   },
+//   {
+//     id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)',
+//   },
+// ];
+
+// //    table
+
+// function EnhancedTableHead(props) {
+//   const {
+//     classes, order, orderBy, onRequestSort,
+//   } = props;
+//   const createSortHandler = (property) => (event) => {
+//     onRequestSort(event, property);
+//   };
+
+//   return (
+//     <TableHead>
+//       <TableRow>
+//         {headCells.map((headCell) => (
+//           <TableCell
+//             key={headCell.id}
+//             align={headCell.numeric ? 'right' : 'left'}
+//             padding={headCell.disablePadding ? 'none' : 'default'}
+//             sortDirection={orderBy === headCell.id ? order : false}
+//           >
+//             <TableSortLabel
+//               active={orderBy === headCell.id}
+//               direction={orderBy === headCell.id ? order : 'asc'}
+//               onClick={createSortHandler(headCell.id)}
+//             >
+//               {headCell.label}
+//               {orderBy === headCell.id ? (
+//                 <span className={classes.visuallyHidden}>
+//                   {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+//                 </span>
+//               ) : null}
+//             </TableSortLabel>
+//           </TableCell>
+//         ))}
+//       </TableRow>
+//     </TableHead>
+//   );
+// }
+
+// EnhancedTableHead.propTypes = {
+//   classes: PropTypes.object.isRequired,
+//   onRequestSort: PropTypes.func.isRequired,
+//   order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+//   orderBy: PropTypes.string.isRequired,
+// };
+
+// // /////////////////////////////////////////////////////////////////////////////////////////
+
+// // //////////////////////////////traine list//////////////////////////////////////////////////
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     width: '100%',
+//   },
+//   paper: {
+//     width: '100%',
+//     marginBottom: theme.spacing(2),
+//   },
+//   table: {
+//     minWidth: 750,
+//   },
+//   visuallyHidden: {
+//     border: 0,
+//     clip: 'rect(0 0 0 0)',
+//     height: 1,
+//     margin: -1,
+//     overflow: 'hidden',
+//     padding: 0,
+//     position: 'absolute',
+//     top: 20,
+//     width: 1,
+//   },
+// }));
+
+// export default function EnhancedTable() {
+//   const classes = useStyles();
+//   const [order, setOrder] = React.useState('asc');
+//   const [orderBy, setOrderBy] = React.useState('calories');
+//   const [selected, setSelected] = React.useState([]);
+
+//   const handleRequestSort = (event, property) => {
+//     const isAsc = orderBy === property && order === 'asc';
+//     setOrder(isAsc ? 'desc' : 'asc');
+//     setOrderBy(property);
+//   };
+
+//   const handleSelectAllClick = (event) => {
+//     if (event.target.checked) {
+//       const newSelecteds = rows.map((n) => n.name);
+//       setSelected(newSelecteds);
+//       return;
+//     }
+//     setSelected([]);
+//   };
+
+//   const handleClick = (event, name) => {
+//     const selectedIndex = selected.indexOf(name);
+//     let newSelected = [];
+
+//     if (selectedIndex === -1) {
+//       newSelected = newSelected.concat(selected, name);
+//     } else if (selectedIndex === 0) {
+//       newSelected = newSelected.concat(selected.slice(1));
+//     } else if (selectedIndex === selected.length - 1) {
+//       newSelected = newSelected.concat(selected.slice(0, -1));
+//     } else if (selectedIndex > 0) {
+//       newSelected = newSelected.concat(
+//         selected.slice(0, selectedIndex),
+//         selected.slice(selectedIndex + 1),
+//       );
+//     }
+
+//     setSelected(newSelected);
+//   };
+
+//   const isSelected = (name) => selected.indexOf(name) !== -1;
+//   return (
+//     <div className={classes.root}>
+//       <Paper className={classes.paper}>
+//         <TableContainer>
+//           <Table
+//             className={classes.table}
+//             aria-labelledby="tableTitle"
+//             aria-label="enhanced table"
+//           >
+//             <EnhancedTableHead
+//               classes={classes}
+//               numSelected={selected.length}
+//               order={order}
+//               orderBy={orderBy}
+//               onSelectAllClick={handleSelectAllClick}
+//               onRequestSort={handleRequestSort}
+//             />
+//             <TableBody>
+//               {stableSort(rows, getComparator(order, orderBy))
+//                 .map((row, index) => {
+//                   const isItemSelected = isSelected(row.name);
+//                   const labelId = `enhanced-table-checkbox-${index}`;
+
+//                   return (
+//                     <TableRow
+//                       hover
+//                       onClick={(event) => handleClick(event, row.name)}
+//                       aria-checked={isItemSelected}
+//                       key={row.name}
+//                       selected={isItemSelected}
+//                     >
+//                       <TableCell component="th" id={labelId} scope="row" padding="none">
+//                         {row.name}
+//                       </TableCell>
+//                       <TableCell align="right">{row.calories}</TableCell>
+//                       <TableCell align="right">{row.fat}</TableCell>
+//                     </TableRow>
+//                   );
+//                 })}
+//             </TableBody>
+//           </Table>
+//         </TableContainer>
+//       </Paper>
+//     </div>
+//   );
+// }
